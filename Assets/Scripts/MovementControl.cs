26b9130f71a1f72fbb98f2732015a4db8d2ff45c
David-Ammon-Student <daama6e@student.oaisd.org>
2026-02-25 13:10:08 -0500

    Final changes, added face direction to movement, jump.

--- Assets/Scripts/MovementControl.cs
+++ Assets/Scripts/MovementControl.cs
@@ -1,26 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementControl : MonoBehaviour
 {
-
-public float moveSpeed = 1.0f;
+    public Rigidbody rigidbody;
+    public float moveSpeed = 1.0f;
+    public float jumpForce = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         if (gameObject.name == "Pidgeon")
         {
-            
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horixontal"); //Interface input going into system
-        transform.position += Vector3.forward * moveSpeed * horizontalInput * Time.deltaTime; //vector mean- representing the world position of out GO 
+        float horizontalInput = Input.GetAxis("Horizontal"); //Interface input going into system
+        Vector3 moveDirection = Vector3.forward * moveSpeed * horizontalInput * Time.deltaTime; //vector mean- representing the world position of out GO 
+        rigidbody.AddForce(moveDirection, ForceMode.Acceleration);
+
+        if (Input.GetKeyDown(KeyCode.Space)) //Pressed the key to jump
+        {
+            Vector3 jumpDirection = Vector3.up * jumpForce;
+            rigidbody.AddForce(jumpDirection, ForceMode.VelocityChange);
+        }
+
+        Vector3 velocity = rigidbody.velocity;
+        velocity.y = 0.0f; //Constant Value here
+        if (velocity.magnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(velocity);
+        }
 
     }
 }
